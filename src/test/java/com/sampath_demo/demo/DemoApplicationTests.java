1a041a77aa204940f2736239ac7cd195ec62a698
savindap-mit <savindap@mitesp.com>
2024-12-24 15:30:31 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/sampath_demo/demo/DemoApplicationTests.java
+++ src/test/java/com/sampath_demo/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.sampath_demo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
